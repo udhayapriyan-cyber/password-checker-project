136b23e5407eb7034e1b9466d130a0403a97af28
udhayapriyan-cyber <udhayapriyancyber@gmail.com>
2026-09-01 11:57:13 +0530

    Implement password strength checking functions
    Added functions to check password strength criteria including length, lowercase, uppercase, and numbers.

---
+++ strength-check.py
@@ -0,0 +1,32 @@
+def length(password):
+  if len(password)>15:
+    return True
+  return False
+def lowercase(password):
+  for ch in password:
+    if ch.islower():
+      return True
+  return False
+def uppercase(password):
+  for ch in password:
+    if ch.isupper():
+      return True
+  return False
+def numbers(password):
+  for ch in password:
+    if ch.isdigit():
+      return True
+  return False
+def check_strength(password):
+  score=0
+  if length(password):
+    score+=1
+  if lowercase(password):
+    score+=1
+  if uppercase(password):
+    score+=1
+  if numbers(password):
+    score+=1
+  if special_characters(password):
+    score+=1
+  return score
